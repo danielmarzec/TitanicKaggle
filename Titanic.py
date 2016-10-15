d78c79a2b14c2e95e415e08c26b8df61b620eced
CarsonBrown96 <carsonbrown67@gmail.com>
2016-10-15 17:37:01 -0400

    Add files via upload

--- Titanic.py
+++ Titanic.py
@@ -7,6 +7,7 @@ train_url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/train.csv
 test_url =  "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/test.csv"
 
 #creating training dataframe
+#Making colab
 train=pd.read_csv(train_url)
 test=pd.read_csv(test_url)
 
