8c4565d8d320301c5830761240c4e73d5c4fa117
danielmarzec <danieljanmarzec@A-MacBook-Pro.local>
2016-10-15 19:22:46 -0400

    Adding features

--- CleaningData.py
+++ CleaningData.py
@@ -1,22 +1,4 @@
 
-# coding: utf-8
-
-# # import pandas as pd
-# import matplotlib.pyplot as plt
-# from sklearn import linear_model
-# from sklearn import ensemble
-# import seaborn as sns
-# # Ensures graphs to be displayed in ipynb
-# %matplotlib inline 
-# #loading train data
-# train_url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/train.csv"
-# # read data into dataframe
-# titanic_df = pd.read_csv(train_url,header=0)  # Always use header=0 to read header of csv files
-# titanic_df
-# 
-# 
-
-# In[42]:
 
 # Import Libraries 
 import pandas as pd
@@ -34,37 +16,30 @@ test_url =  "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/test.csv"
 titanic_df = pd.read_csv(train_url,header=0)  # Always use header=0 to read header of csv files
 test = pd.read_csv(test_url,header = 0)
 
-
-# In[43]:
-
+#print amount of missing values in train and test
 train.isnull().sum()
 
-
-# In[44]:
-
 test.isnull().sum()
-
-
-# In[45]:
 
 #set null values in 'Embarked' to 'C'
 _ = train.set_value(train.Embarked.isnull(), 'Embarked', 'C')
 
-
-# In[46]:
-
 #set null values in fare to 8.05, most common fare value
 _ = test.set_value(test.Fare.isnull(), 'Fare', 8.05)
-
-
-# In[47]:
 
 #replace missing values of Cabin with U0
 full = pd.concat([train, test], ignore_index=True)
 _ = full.set_value(full.Cabin.isnull(), 'Cabin', 'U0')
 
 
-# In[48]:
+#create feature called group = sibSP + parch + 1
+full['Group_num'] = full.Parch + full.SibSp + 1
+
+#create feautre called Group_size where Group_size>4 = 'L' and <4 = 'S'
+#More people survived when size was between 2 and 4
+full['Group_size'] = pd.Series('M', index=full.index)
+_ = full.set_value(full.Group_num>4, 'Group_size', 'L')
+_ = full.set_value(full.Group_num==1, 'Group_size', 'S')
 
 
 
